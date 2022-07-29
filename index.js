c92d1eebb3c4afdfd2972598488548da232e7d13
Igor <igor.kurochkin@succraft.com>
2022-07-29 16:27:49 +0300

    add redirect

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const jsforce = require('jsforce');
-
+const redir = process.env.REDIRECT_URI || "http://localhost:" + PORT + "/token";
+console.log('redir:', redir);
 const {SF_LOGIN_URL, SF_USERNAME, SF_PASSWORD} = process.env;
 const app = new Express();
 app.use(Express.json());
@@ -15,24 +16,24 @@ conn.login(SF_USERNAME, SF_PASSWORD, (error, userInfo) => {
     if (error) {
         console.error(error);
     } else {
-        console.log('User Id: ', userInfo.id);
-        console.log('Org Id: ', userInfo.organizationId);
-        console.log(conn.accessToken);
-        console.log(conn.instanceUrl);
+        // console.log('User Id: ', userInfo.id);
+        // console.log('Org Id: ', userInfo.organizationId);
+        // console.log(conn.accessToken);
+        // console.log(conn.instanceUrl);
     }
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     res.status(200).json('Server worked');
 })
 
 app.get('/', (req, res) => {
     conn.query("SELECT Id, Name FROM Account", function(err, result) {
         if (err) { return console.error(err); }
-        console.log("total : " + result.totalSize);
-        console.log("fetched : " + result.records.length);
-        console.log("total : ", result.records);
+        // console.log("total : " + result.totalSize);
+        // console.log("fetched : " + result.records.length);
+        // console.log("total : ", result.records);
         res.status(200).json(result.records);
     });
 
